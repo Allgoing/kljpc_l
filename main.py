47cf829652750148d1d774d9f4ee777b3db7d875
lihang <573447533@qq.com>
2021-03-23 10:22:48 +0800

    配置

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ import pytest
 
 pytest.main(['-s', '-v', '-rerun=3', '--alluredir=./report/allure'])
 os.system(r'allure generate ./report/allure -o ./report/html --clean')
-shutil.make_archive('aaa', 'zip', root_dir=r'./report/html')
+# shutil.make_archive('aaa', 'zip', root_dir=r'./report/html')
